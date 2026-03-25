aae1efd4311fd793b289df65d3826c24a15a6856
Aryan Mishra <189445552+aryan00719@users.noreply.github.com>
2026-03-25 16:28:14 +0530

    Refactor JD parser to support required/good-to-have sections and implement weighted skill matching

--- utils/jd_parser.py
+++ utils/jd_parser.py
@@ -1,21 +1,23 @@
-from utils.resume_parser import clean_text
+from utils.resume_parser import clean_text, extract_skills
 from utils.skill_normalizer import normalize_skills
-
-JD_SKILL_KEYWORDS = [
-    "react", "reactjs",
-    "javascript", "js",
-    "machine learning", "ml",
-    "deep learning", "dl",
-    "python", "java", "sql", "flask"
-]
 
 def parse_job_description(jd_text):
     cleaned = clean_text(jd_text)
-    found_skills = []
+    
+    req_idx = cleaned.find("required")
+    good_idx = cleaned.find("good to have")
+    
+    if good_idx != -1:
+        if req_idx != -1 and req_idx < good_idx:
+            req_text = cleaned[req_idx:good_idx]
+        else:
+            req_text = cleaned[:good_idx]
+        good_text = cleaned[good_idx:]
+    else:
+        req_text = cleaned
+        good_text = ""
 
-    for skill in JD_SKILL_KEYWORDS:
-        if skill in cleaned:
-            found_skills.append(skill)
-
-    # normalize AFTER extraction
-    return normalize_skills(found_skills)
+    return {
+        "required": normalize_skills(extract_skills(req_text)),
+        "good_to_have": normalize_skills(extract_skills(good_text)) if good_text else []
+    }
